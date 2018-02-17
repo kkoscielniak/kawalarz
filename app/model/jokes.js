306ee7a1cfd176305a13cb0da8a025581c78d97f
Krystian Kościelniak <krystiankoscielniak@gmail.com>
2018-02-17 16:21:53 +0100

    Add some jokes

--- app/model/jokes.js
+++ app/model/jokes.js
@@ -7,4 +7,100 @@ module.exports = [
     question: 'Ile nici ma dentysta?',
     answer: 'Stomatologiczne',
   },
+  {
+    question: 'Dlaczego miarkę wpuszczono na imprezę, a inne narzędzia nie?',
+    answer: 'Bo się przebrała',
+  },
+  {
+    question: 'Czego rudy szuka w kopalni?',
+    answer: 'Rudy miedzi',
+  },
+  {
+    question: 'Jak można powiedzieć na dzieci rudego?',
+    answer: 'Marudy',
+  },
+  {
+    question: 'Patrzy facet przez okno a ktos biegnie. A to...',
+    answer: 'Droga do lasu',
+  },
+  {
+    question: 'Patrzy facet przez okno i słyszy jakies kroki. A tam...',
+    answer: 'Przeszło ludzkie pojęcie',
+  },
+  {
+    question: 'Jak nazywa się skok Stefana Huli?',
+    answer: 'Hula-hop',
+  },
+  {
+    question: 'Jak nazywa się zwierzę na dwie litery?',
+    answer: 'Qń.',
+  },
+  {
+    question: 'Jak nazywa się państwo na jedną literę?',
+    answer: 'Samoa',
+  },
+  {
+    question: 'Jak nazywają się podejrzane rejony włoskiego miasta?',
+    answer: 'Spaghetto',
+  },
+  {
+    question: 'Dlaczego wypłata jest tylko złudzenem?',
+    answer: 'Bo nam się wydaje',
+  },
+  {
+    question: 'Jak nazywa się dziewczyna, na którą ktos rzucił urok?',
+    answer: 'Zauroczona',
+  },
+  {
+    question: 'Jak nazywa się doktor leczący pandy?',
+    answer: 'Pandoktor',
+  },
+  {
+    question: 'Jakie zwierzę jest najlepszym szpiegiem?',
+    answer: 'Sledź',
+  },
+  {
+    question: 'Co robi Putin na odpornosc?',
+    answer: 'Putinoscorbin',
+  },
+  {
+    question: 'Jaki jest magik gdy straci magię?',
+    answer: 'Rozczarowany',
+  },
+  {
+    question: 'Jakie jest imię greckiego boga czystosci?',
+    answer: 'Domestos',
+  },
+  {
+    question: 'Dlaczego klatka piersiowa ma grosze?',
+    answer: 'Bo żebra',
+  },
+  {
+    question: 'Jaki urząd nigdy nie pudłuje?',
+    answer: 'Urząd celny',
+  },
+  {
+    question: 'Co mówi kura do sowy?',
+    answer: 'Kokosowo',
+  },
+  {
+    question: 'Dlaczego krowa nie zdała do następnej klasy?',
+    answer: 'Bo robiła dużo byków',
+  },
+  {
+    question: 'Jak nazywa się kraj, w którym nie wolno palic papierosów?',
+    answer: 'Nepal',
+  },
+  {
+    question: 'Jakie kino lubią maklerzy?',
+    answer: 'Kino akcji',
+  },
+  {
+    question: 'Jak nazywa się ptak, który stoi w drzwiach?',
+    answer: 'Indor',
+  },
+  {
+    question: 'Co robi siatkarz, gdy ma urodziny?',
+    answer: 'Przyjęcie',
+  },
 ];
